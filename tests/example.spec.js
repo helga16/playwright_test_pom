6140316f69d5d6720f1cb4363588776093475f27
kaplunova <melamori1605@gmail.com>
2024-12-30 12:00:42 +0200

    changes to tests

--- tests/example.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Conduit home page has a title', async ({ page }) => {
-  await page.goto('https://conduit.mate.academy');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Conduit/);
-});
